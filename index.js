7cb6efd338742718a0761b7b9b9f96b8f92dce2f
TanninOne <seppleviathan@gmx.de>
2020-11-13 15:39:24 +0100

    rewritten to make use of winapi-bindings

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const winapi = require('winapi-bindings');
+
 function getVersion(filePath) {
   if (process.platform === 'win32') {
-    return require('./build/Release/ExeVersion').getVersion(filePath);
+    const v = winapi.GetFileVersionInfo(filePath).fileVersion;
+    return `${v[0]}.${v[1]}.${v[2]}.${v[3]}`;
   }
   // not implemented on MacOS and Linux. On MacOS we could use the bundle version
   // from the plist file. On Linux? Hmm
