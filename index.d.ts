57b04b6867077b5afd59ea02d0ec6b755b861e5c
TanninOne <seppleviathan@gmx.de>
2021-11-19 12:55:43 +0100

    added support for fetching localized version info

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,6 @@ declare module 'exe-version' {
   export default function getVersion(exeFile: string);
   export function getFileVersion(exeFile: string);
   export function getProductVersion(exeFile: string);
+  export function getFileVersionLocalized(exeFile: string);
+  export function getProductVersionLocalized(exeFile: string);
 }
-
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const winapi = require('winapi-bindings');
 
-function getVersion(filePath) {
+function getFileVersion(filePath) {
   if (process.platform === 'win32') {
     const v = winapi.GetFileVersionInfo(filePath).fileVersion;
     return `${v[0]}.${v[1]}.${v[2]}.${v[3]}`;
@@ -18,10 +18,40 @@ function getProductVersion(filePath) {
   return '';
 }
 
+function getFileVersionLocalized(filePath) {
+  if (process.platform === 'win32') {
+    return winapi.GetFileVersionInfo(filePath).fileVersionString;
+  } else {
+    // windows PEs store "fixed" file/product versions plus potentially another 2 versions,
+    // in a different format, for an arbitrary number of localizations, including potentially
+    // localizations for invalid languages.
+    // All are completely independent.
+    // The windows explorer then shows the fixed >file< version but the localized >product< version
+    // for some reason.
+    // Surely other OSes don't have something
+    // this.
+    // f***ing.
+    // stupid.
+    // right?
+    return getFileVersion();
+  }
+}
+
+function getProductVersionLocalized(filePath) {
+  if (process.platform === 'win32') {
+    return winapi.GetFileVersionInfo(filePath).productVersionString;
+  } else {
+    // ...
+    return getProductVersion();
+  }
+}
+
 module.exports = {
   __esModule: true,
-  default: getVersion,
-  getFileVersion: getVersion,
+  default: getFileVersion,
+  getFileVersion,
   getProductVersion,
+  getFileVersionLocalized,
+  getProductVersionLocalized,
 }
 
